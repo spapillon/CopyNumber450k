a85d20e04fa4203e14d844199dd4393dc0346b9d
Simon Papillon <papillonsimon@gmail.com>
2013-09-04 16:45:32 -0400

    Fixed messed up indent

--- formatCNVObject.R
+++ formatCNVObject.R
@@ -19,38 +19,37 @@ formatCNVObject <- function(CNAobject, sample_intensity, control_intensity, site
 	site_annotation <- site_annotation[rownames(sample_intensity), ]
 	
 	x <- lapply(1:length(segments_per_sample), function(i) {
-				result <- t(apply(segments_per_sample[[i]], 1, function(cnv) {
-									# Extract probes
-									probes <- site_annotation$CHR == cnv['chrom'] &
-											as.numeric(site_annotation$MAPINFO) >= as.numeric(cnv['loc.start']) &
-											as.numeric(site_annotation$MAPINFO) <= as.numeric(cnv['loc.end'])
+			result <- t(apply(segments_per_sample[[i]], 1, function(cnv) {
+				# Extract probes
+				probes <- site_annotation$CHR == cnv['chrom'] &
+					as.numeric(site_annotation$MAPINFO) >= as.numeric(cnv['loc.start']) &
+					as.numeric(site_annotation$MAPINFO) <= as.numeric(cnv['loc.end'])
 									
-									# Compute segment values
-									segment_control_int_sum <- colSums(control_intensity[probes,])
-									segment_sample_sum <- sum(sample_intensity[probes,i])
-									segment_log_ratio <- log2(segment_sample_sum / mean(segment_control_int_sum))
+				# Compute segment values
+				segment_control_int_sum <- colSums(control_intensity[probes,])
+				segment_sample_sum <- sum(sample_intensity[probes,i])
+				segment_log_ratio <- log2(segment_sample_sum / mean(segment_control_int_sum))
 									
-									# Compute p-value (2 sided t-test)
-									segment_z_score <- (segment_sample_sum - mean(segment_control_int_sum)) / sd(segment_control_int_sum)
-									segment_p_value <-  2 * pnorm(-abs(segment_z_score))
+				# Compute p-value (2 sided t-test)
+				segment_z_score <- (segment_sample_sum - mean(segment_control_int_sum)) / sd(segment_control_int_sum)
+				segment_p_value <-  2 * pnorm(-abs(segment_z_score))
 									
-									# Extract genes in the segment
-									genes <- as.character(site_annotation$UCSC_RefGene_Name[probes])
-									genes <- unique(unlist(strsplit(x=genes, ";")))
-									genes <- paste(genes, collapse=";")
-									l <- as.numeric(cnv['loc.end']) - as.numeric(cnv['loc.start'])
-									
-									return(c(cnv, seg.length=l, logratio=segment_log_ratio, pvalue=segment_p_value, genes=genes))
-								}))
+				# Extract genes in the segment
+				genes <- as.character(site_annotation$UCSC_RefGene_Name[probes])
+				genes <- unique(unlist(strsplit(x=genes, ";")))
+				genes <- paste(genes, collapse=";")
+				l <- as.numeric(cnv['loc.end']) - as.numeric(cnv['loc.start'])
+
+				return(c(cnv, seg.length=l, logratio=segment_log_ratio, pvalue=segment_p_value, genes=genes))
+			}))
 				
-				# pvalue correction for multiple testing
-				result <- as.data.frame(result)
-				result$adjusted.pvalue <- p.adjust(result$pvalue, method=p.adjust.method)
+			# pvalue correction for multiple testing
+			result <- as.data.frame(result)
+			result$adjusted.pvalue <- p.adjust(result$pvalue, method=p.adjust.method)
 				
-				if(verbose)
-					message(paste("Processed", names(segments_per_sample)[i]))
-				return(result)
-			})
-	
+			if(verbose)
+				message(paste("Processed", names(segments_per_sample)[i]))
+			return(result)
+		})
 	return(x)
 }
