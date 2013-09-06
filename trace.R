9e62ada05a27a9c927dd4fcaea8ad29de9b1c1ef
Simon Papillon <papillonsimon@gmail.com>
2013-09-06 12:43:15 -0400

    Added SNP probe removal to trace

--- trace.R
+++ trace.R
@@ -12,6 +12,7 @@ source('FunNormCN450k.R')
 
 path <- '~/Documents/iChange/data_ETMR'
 RGset <- read.450k.exp(base = path, targets = read.450k.sheet(path))
+
 # normalize (JP's new thing)
 extracted_data <- extractFromRGSet450k(RGset)
 mset <- preprocessRaw(RGset)
@@ -19,7 +20,11 @@ cnMatrix <- getMeth(mset) + getUnmeth(mset)
 sexes <- predictSex(extractedData = extracted_data, cutoff = 0)
 cnMatrix.norm <- normalizeFunNorm450kCN(cnMatrix = cnMatrix, extractedData = extracted_data, predictedSex = sexes)
 
-# filter probes (verify utility)
+rm(sexes)
+
+# define phenotypes
+
+# filter probes (verify utility) based on variance
 con.intensity.median <- rowMedians(cnMatrix[, phenotype == "control"])
 median.quantiles <- quantile(con.intensity.median, c(0.05, 0.85), na.rm=TRUE)
 
@@ -30,7 +35,22 @@ usable_probes <- con.intensity.median >= median.quantiles[1] &
 	con.intensity.median <= median.quantiles[2] &
 	con.intensity.mad <= mad.quantiles
 
+rm(con.intensity.media, median.quantiles, con.intensity.mad, mad.quantiles)
+
+# filter SNP probes
+require(GEOquery)
+site_annotation = Table(getGEO('GPL13534') )
+rownames(site_annotation) <- site_annotation$ID
+site_annotation <- site_annotation[rownames(cnMatrix.norm), ]
+non_snp_probes <- site_annotation$Probe_SNPs == "" & site_annotation$Probe_SNPs_10  == ""
+
+good_probes <- usable_probes & non_snp_probes
+rm(usable_probes, non_snp_probes)
+
 # log2
+CNVobject <- createCNVObject.R(cnMatrix, phenotype, good_probes)
+
+
 # segmentation
 # formatting
 # visualize
