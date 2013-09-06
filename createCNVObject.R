8b6d87e46db234fbf9a1a91b14f209fa29bb42b2
Simon Papillon <papillonsimon@gmail.com>
2013-09-06 19:03:07 -0400

    Minor indexing correction

--- createCNVObject.R
+++ createCNVObject.R
@@ -4,12 +4,13 @@
 ###############################################################################
 
 
-createCNVObject <- function(cnMatrix, phenotype, good_probes) {
+createCNVObject <- function(cnMatrix, phenotype, good_probes, site_annotation) {
 	library(DNAcopy)
 	cnMatrix <- cnMatrix[good_probes, ]
+	site_annotation <- site_annotation[good_probes, ]
 	control_medians <- rowMeans(cnMatrix[, phenotype == "control"])
 	cases_log2 <- log2(cnMatrix[, phenotype != "control"] / control_medians)
-	CNA.object <- CNA(cases_log2, ordered(site_annotation$CHR)[usable_probes], as.numeric(site_annotation$MAPINFO)[usable_probes], data.type="logratio" )
+	CNA.object <- CNA(cases_log2, ordered(site_annotation$CHR), as.numeric(site_annotation$MAPINFO), data.type="logratio" )
 	
 	smoothed.CNA.object <- smooth.CNA(CNA.object)
 	segment.smoothed.CNA.object <- segment(smoothed.CNA.object, min.width=5 ,verbose=1, nperm=10000, alpha=0.01, undo.splits="sdundo", undo.SD=2)
