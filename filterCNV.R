71d8c88616afe6d74c9f29392917d8b1aa8ab3cb
Simon Papillon <papillonsimon@gmail.com>
2013-09-07 10:24:11 -0400

    Fixed typo

--- filterCNV.R
+++ filterCNV.R
@@ -4,7 +4,7 @@
 ###############################################################################
 
 
-filterCVN <- function(CNVformat, tick.threshold=50, pvalue.threshold=0.01, breakpoints=c(0,0,0,0)) {
+filterCNV <- function(CNVformat, tick.threshold=50, pvalue.threshold=0.01, breakpoints=c(0,0,0,0)) {
 	lapply(CNVformat, function(sample) as.numeric(sample[,'num.mark']) >= tick.threshold &
 						as.numeric(sample[,'adjusted.pvalue']) <= pvalue.threshold)
 }
