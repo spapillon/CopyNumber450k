a79c3c86c96413f364969f1455e36f474574bd4e
Simon Papillon <papillonsimon@gmail.com>
2013-09-06 22:36:30 -0400

    Added filterCNV

---
+++ filterCNV.R
@@ -0,0 +1,11 @@
+# TODO: Add comment
+# 
+# Author: spapillo
+###############################################################################
+
+
+filterCVN <- function(CNVformat, tick.threshold=50, pvalue.threshold=0.01, breakpoints=c(0,0,0,0)) {
+	lapply(CNVformat, function(sample) as.numeric(sample[,'num.mark']) >= tick.threshold &
+						as.numeric(sample[,'adjusted.pvalue']) <= pvalue.threshold)
+}
+
