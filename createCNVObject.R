9b1c1558b27e8b41664768696c9688aa10cd179f
Simon Papillon <papillonsimon@gmail.com>
2013-09-07 11:08:20 -0400

    More name persistence

--- createCNVObject.R
+++ createCNVObject.R
@@ -10,7 +10,8 @@ createCNVObject <- function(cnMatrix, phenotype, good_probes, site_annotation) {
 	site_annotation <- site_annotation[good_probes, ]
 	control_medians <- rowMeans(cnMatrix[, phenotype == "control"])
 	cases_log2 <- log2(cnMatrix[, phenotype != "control"] / control_medians)
-	CNA.object <- CNA(cases_log2, ordered(site_annotation$CHR), as.numeric(site_annotation$MAPINFO), data.type="logratio" )
+	CNA.object <- CNA(cases_log2, ordered(site_annotation$CHR), as.numeric(site_annotation$MAPINFO),
+			data.type="logratio", sampleid=colnames(cases_log2))
 	
 	smoothed.CNA.object <- smooth.CNA(CNA.object)
 	segment.smoothed.CNA.object <- segment(smoothed.CNA.object, min.width=5 ,verbose=1, nperm=10000, alpha=0.01, undo.splits="sdundo", undo.SD=2)
