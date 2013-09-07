8aacef18a149f3b76463d520d6cd13072d46f405
Simon Papillon <papillonsimon@gmail.com>
2013-09-06 22:38:24 -0400

    Corrected naming error

--- intersectCNV.R
+++ intersectCNV.R
@@ -5,7 +5,7 @@
 
 
 
-CNVintersect <- function(CNVformat, filter) {
+intersectCNV <- function(CNVformat, filter) {
 	x <- unlist(sapply(1:length(CNVformat), function(i) {
 						unique(unlist(strsplit(x=CNVformat[[i]][filter[[i]],'genes'], ";")))
 					}))
