bf32f802395f1d46fad6fe22b8701418f4e9195a
Simon Papillon <papillonsimon@gmail.com>
2013-09-08 18:25:21 -0400

    Added gain/loss support

--- findCNV.R
+++ findCNV.R
@@ -4,15 +4,21 @@
 ###############################################################################
 
 
-findCNV <- function(CNVformat, CNVfilter, CNVs) {
+findCNV <- function(CNVformat, CNVfilter, CNVs, type="both") {
 	
 	x <- sapply(1:length(CNVformat), function(i) {
-						genes <- unique(unlist(strsplit(x=CNVformat[[i]][filter[[i]],'genes'], ";")))
-						return(CNVs %in% genes)
-					})
+				if(type == "both")
+					used_CNVs <- filter[[i]]
+				else if(type == "gain")
+					used_CNVs <- CNVformat[[i]][,'logratio'] > 0 & filter[[i]]
+				else if(type == "loss")
+					used_CNVs <- CNVformat[[i]][,'logratio'] < 0 & filter[[i]]
+				
+				genes <- unique(unlist(strsplit(x=CNVformat[[i]][used_CNVs,'genes'], ";")))
+				return(CNVs %in% genes)
+			})
 
 	ifelse(is.matrix(x), x <- t(x), x <- as.matrix(x))
-	browser()
 	rownames(x) <- names(CNVformat)
 	colnames(x) <- CNVs
 	return(x)
