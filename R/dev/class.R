ecd7f7d2ae2ebd7706fee1251b0e09079c1fda9b
Nicolas De Jay <ndj@pinkfilter.org>
2013-10-26 19:55:39 -0400

    Fixed minor bug related to sex prediction at initialization time

--- R/dev/class.R
+++ R/dev/class.R
@@ -92,11 +92,13 @@ MethylCNVDataSetFromRGChannelSet <- function(RGChannelSet) {
     # TODO: Equipment-generated variables describing sample phenotypes
     # (AnnotatedDataFrame-class): protocolData
     
-    browser()
-    
-    new("MethylCNVDataSet", summary = summary, assayData = assayData, phenoData = phenoData, 
+    object <- new("MethylCNVDataSet", summary = summary, assayData = assayData, phenoData = phenoData, 
         featureData = featureData, annotation = annotation, manifest = manifest, 
         segments = list())
+
+    # TODO: Weird to put it here... should predictSampleSex just be a general function and not a method?
+    pData(object)$Sample_Sex <- predictSampleSexes(object)
+    object
 }
 
 ################################################################################  
