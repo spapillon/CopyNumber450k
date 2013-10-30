8e37c3c20a4c78a254328c81791527bcd96e977a
Simon Papillon <papillonsimon@gmail.com>
2013-10-30 18:18:19 -0400

    Added dropSNPprobes in debug

--- debug.R
+++ debug.R
@@ -2,18 +2,18 @@
 library(CopyNumber450k)
 load("~/git/CopyNumber450k/data/control_RGset.RData")
 
-pData(control_RGset)$Sample_Group[1] <- "case"
-
+#pData(control_RGset)$Sample_Group[1] <- "case"
+path <- "~/Documents/iChange/data_ETMR/"
+case_RGset <- read.450k.exp(base = path, targets = read.450k.sheet(path))
+#pData(case_RGset)$Sample_Group[-c(1,2)]  <- "control"
 control_RGset <- updateObject(control_RGset)
-
-mcds <- CNV450kSet(control_RGset)
-
-
-
-pData(mcds)$Sample_Sex <- predictSampleSexes(mcds)
-
-mcds.f <- normalize(mcds, "functional")
+RGset <- combine(control_RGset, case_RGset)
+mcds <- CNV450kSet(RGset)
+mcds <- dropSNPprobes(RGset)
+#mcds.f <- normalize(mcds, "functional")
 mcds.q <- normalize(mcds, "quantile")
+mcds.q.seg <- segmentize(mcds.q)
+
 par(mfrow=c(1,3))
 plotDensity(mcds)
 plotDensity(mcds.f)
