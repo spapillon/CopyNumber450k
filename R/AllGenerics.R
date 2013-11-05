993715ee2094ef7a08ed49a17732c30c8c0b31fa
Nicolas De Jay <ndj@pinkfilter.org>
2013-11-05 17:40:04 -0500

    Commented out lines in the vignette as it is broken (data retrieval)

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -24,6 +24,10 @@ setGeneric("dropSNPprobes", function(object, maf_threshold = 0) standardGeneric(
 
 ################################################################################  
 
+setGeneric("normalize", function(object, ...) standardGeneric("normalize", ...))
+
+################################################################################  
+
 setGeneric("segmentize", function(object, verbose = TRUE, p.adjust.method = "bonferroni") standardGeneric("segmentize"))
 
 ################################################################################  
