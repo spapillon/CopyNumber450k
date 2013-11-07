f3ee8ef8eff399df5244aff4d54f6efed0549462
Simon Papillon <papillonsimon@gmail.com>
2013-11-07 17:08:40 -0500

    Final draft for the vignette

--- debug.R
+++ debug.R
@@ -3,19 +3,14 @@
 ################################################################################  
 
 library(CopyNumber450k)
-load("~/git/CopyNumber450k/data/control_RGset.RData")
+library(CopyNumber450kData)
+library(minfiData)
+data(RGcontrolSetEx)
+data(RGsetEx)
 
-baseDir <- system.file("extdata", package = "minfiData")
-targets <- read.450k.sheet(baseDir)
-case_RGset <- read.450k.exp(base = baseDir, targets = targets)
-control_RGset <- updateObject(control_RGset)
-RGset <- combine(control_RGset, case_RGset)
+RGset <- combine(RGcontrolSetEx, RGsetEx)
+
 mcds <- CNV450kSet(RGset)
 mcds <- dropSNPprobes(mcds)
 mcds.q <- normalize(mcds, "quantile")
-mcds.q.seg <- segmentize(mcds.q)
-
-par(mfrow=c(1,3))
-plotDensity(mcds)
-plotDensity(mcds.f)
-plotDensity(mcds.q)
+mcds.q <- segmentize(mcds.q)
