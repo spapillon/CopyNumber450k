c20c2f0a666f6c0d88fe013a9f5b9b2aeaa2fd03
Simon Papillon <papillonsimon@gmail.com>
2013-11-07 14:54:56 -0500

    Removed obsolete commands in debug

--- debug.R
+++ debug.R
@@ -1,24 +1,3 @@
-################################################################################ 
-library(CopyNumber450k)
-load("~/git/CopyNumber450k/data/control_RGset.RData")
-
-#pData(control_RGset)$Sample_Group[1] <- "case"
-path <- "~/Documents/iChange/data_ETMR/"
-case_RGset <- read.450k.exp(base = path, targets = read.450k.sheet(path))
-#pData(case_RGset)$Sample_Group[-c(1,2)]  <- "control"
-control_RGset <- updateObject(control_RGset)
-RGset <- combine(control_RGset, case_RGset)
-mcds <- CNV450kSet(RGset)
-mcds <- dropSNPprobes(mcds)
-#mcds.f <- normalize(mcds, "functional")
-mcds.q <- normalize(mcds, "quantile")
-mcds.q.seg <- segmentize(mcds.q)
-
-par(mfrow=c(1,3))
-plotDensity(mcds)
-plotDensity(mcds.f)
-plotDensity(mcds.q)
-
 ################################################################################  
 # Using minfiData
 ################################################################################  
@@ -35,3 +14,8 @@ mcds <- CNV450kSet(RGset)
 mcds <- dropSNPprobes(mcds)
 mcds.q <- normalize(mcds, "quantile")
 mcds.q.seg <- segmentize(mcds.q)
+
+par(mfrow=c(1,3))
+plotDensity(mcds)
+plotDensity(mcds.f)
+plotDensity(mcds.q)
