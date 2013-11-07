e47aa3293b06852d935576a8a62df1857132216c
Simon Papillon <papillonsimon@gmail.com>
2013-11-07 14:07:45 -0500

    Fixed minor bug and added miniData version

--- debug.R
+++ debug.R
@@ -9,7 +9,7 @@ case_RGset <- read.450k.exp(base = path, targets = read.450k.sheet(path))
 control_RGset <- updateObject(control_RGset)
 RGset <- combine(control_RGset, case_RGset)
 mcds <- CNV450kSet(RGset)
-mcds <- dropSNPprobes(RGset)
+mcds <- dropSNPprobes(mcds)
 #mcds.f <- normalize(mcds, "functional")
 mcds.q <- normalize(mcds, "quantile")
 mcds.q.seg <- segmentize(mcds.q)
@@ -20,3 +20,18 @@ plotDensity(mcds.f)
 plotDensity(mcds.q)
 
 ################################################################################  
+# Using minfiData
+################################################################################  
+
+library(CopyNumber450k)
+load("~/git/CopyNumber450k/data/control_RGset.RData")
+
+baseDir <- system.file("extdata", package = "minfiData")
+targets <- read.450k.sheet(baseDir)
+case_RGset <- read.450k.exp(base = baseDir, targets = targets)
+control_RGset <- updateObject(control_RGset)
+RGset <- combine(control_RGset, case_RGset)
+mcds <- CNV450kSet(RGset)
+mcds <- dropSNPprobes(mcds)
+mcds.q <- normalize(mcds, "quantile")
+mcds.q.seg <- segmentize(mcds.q)
